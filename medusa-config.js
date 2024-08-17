1c4a9dd26d8d240cd99e1388ce80c0e80266aa7a
Kickbykick <kiibatitoo@gmail.com>
2024-08-16 20:31:00 -0500

    google sign in

--- medusa-config.js
+++ medusa-config.js
@@ -30,11 +30,12 @@ const STORE_URL = process.env.STORE_URL || "localhost:8000"
    
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
-
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
-
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+const GoogleClientId = process.env.GOOGLE_CLIENT_ID || ""
+const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET || ""
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -55,6 +56,46 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: "medusa-plugin-auth",
+    /** @type {import('medusa-plugin-auth').AuthOptions} */
+    options: [
+      {
+        type: "google",
+        // strict: "all", // or "none" or "store" or "admin"
+        strict: "none",
+        identifier: "google",
+        clientID: GoogleClientId,
+        clientSecret: GoogleClientSecret,
+        admin: {
+          callbackUrl: `${BACKEND_URL}/admin/auth/google/cb`,
+          failureRedirect: `${ADMIN_URL}/login`,
+          // The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url` to the auth url
+          // This query param will have the priority over this configuration
+          successRedirect: `${ADMIN_URL}/`,
+          authPath: '/admin/auth/google',
+          authCallbackPath: '/admin/auth/google/cb',
+          expiresIn: 24 * 60 * 60 * 1000,
+          // verifyCallback: (container, req, accessToken, refreshToken, profile, strict) => {
+          //    // implement your custom verify callback here if you need it
+          // },
+        },
+        store: {
+          callbackUrl: `${BACKEND_URL}/store/auth/google/cb`,
+          failureRedirect: `${STORE_URL}/login`,
+          // The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url` to the auth url
+          // This query param will have the priority over this configuration
+          successRedirect: `${STORE_URL}/`,
+          authPath: '/store/auth/google',
+          authCallbackPath: '/store/auth/google/cb',
+          expiresIn: 24 * 60 * 60 * 1000,
+          // verifyCallback: (container, req, accessToken, refreshToken, profile, strict) => {
+          //    // implement your custom verify callback here if you need it
+          // },
+        }
+      }
+    ]
+  }
 ];
 
 const modules = {
