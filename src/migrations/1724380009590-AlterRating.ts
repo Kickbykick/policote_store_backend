502355249c5f6b32a3695464ecadd47315a4c1e9
Kickbykick <kiibatitoo@gmail.com>
2025-02-12 23:32:09 -0600

    fix syntax issues

--- src/migrations/1724380009590-AlterRating.ts
+++ src/migrations/1724380009590-AlterRating.ts
@@ -6,13 +6,13 @@ export class AlterRating1692740800000 implements MigrationInterface {
         await queryRunner.query(`
             ALTER TABLE "rating"
             ADD COLUMN "service_provider_id" character varying,
-            ADD COLUMN "profile_id" character varying,
+            ADD COLUMN "profile_id" character varying
         `)
 
         await queryRunner.query(`
             ALTER TABLE "rating"
             ADD CONSTRAINT "FK_rating_service_provider" FOREIGN KEY ("service_provider_id") REFERENCES "service_provider"("id"),
-            ADD CONSTRAINT "FK_rating_profile" FOREIGN KEY ("profile_id") REFERENCES "profile"("id"),
+            ADD CONSTRAINT "FK_rating_profile" FOREIGN KEY ("profile_id") REFERENCES "profile"("id")
         `)
     }
 
@@ -20,13 +20,13 @@ export class AlterRating1692740800000 implements MigrationInterface {
         await queryRunner.query(`
             ALTER TABLE "rating"
             DROP CONSTRAINT "FK_rating_service_provider",
-            DROP CONSTRAINT "FK_rating_profile",
+            DROP CONSTRAINT "FK_rating_profile"
         `)
 
         await queryRunner.query(`
             ALTER TABLE "rating"
             DROP COLUMN "service_provider_id",
-            DROP COLUMN "profile_id",
+            DROP COLUMN "profile_id"
         `)
     }
 }
